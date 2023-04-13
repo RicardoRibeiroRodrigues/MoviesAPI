a1eb5a02f84d3122f0261d2a4f08d34aec671ed6
RicardoRibeiroRodrigues <riywar@gmail.com>
2023-04-13 15:19:00 -0300

    Documenta os modelos

--- models.py
+++ models.py
@@ -2,25 +2,43 @@ from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
-    user_id: int
-    username: str
-    full_name: str | None = None
+    user_id: int = Field(description="The ID of the user in the database.", example=1)
+    username: str = Field(description="The username of the user.", example="user123")
+    full_name: str | None = Field(
+        default=None,
+        description="The full name of the user.",
+        example="User 123 da Silva",
+    )
 
 
 class movieReview(BaseModel):
     n_stars: float = Field(
-        gte=0.0, le=10.0, description="The number of stars this movie got"
+        gte=0.0,
+        le=5.0,
+        description="The number of stars from 0.0 to 5.0 the user has given to the movie.",
+        example=4.6,
     )
-    review: str
-    user: User
+    review: str = Field(
+        description="The texto of the review of the movie.", example="Great movie!"
+    )
+    user: User = Field(
+        description="The user that has made the review.",
+    )
 
 
 class Movie(BaseModel):
-    title: str
-    description: str | None = None
-    year: int
+    title: str = Field(description="The title of the movie.", example="The Matrix")
+    studio: str = Field(
+        description="The studio that produced the movie.", example="Warner Bros."
+    )
+    description: str | None = Field(
+        default=None,
+        description="The description of the movie.",
+        example="A movie about a hacker.",
+    )
+    year: int = Field(description="The year the movie was released.", example=1999)
 
 
 class MovieDB(Movie):
-    movie_id: int
+    movie_id: int = Field(description="The ID of the movie in the database.")
     reviews: list[movieReview] | list = []
