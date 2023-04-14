ecf06fbdf8a7f07ab34f4358d0743a4076ddd9af
RicardoRibeiroRodrigues <riywar@gmail.com>
2023-04-13 22:09:05 -0300

    Adiciona pontos RESTful e muda db para um dicionario

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from typing import Annotated
 from models import *
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Response, status, Body
 
 app = FastAPI()
 
-movies: list[MovieDB] = []
+movies: dict[int, MovieDB] = {}
+last_id = 0
+
+
+def add_movie(movie: MovieDB) -> int:
+    global last_id
+    movie_id = last_id
+    movies[movie_id] = movie
+    last_id += 1
+    return movie_id
+
+
+def remove_movie(movie_id: int) -> MovieDB:
+    return movies.pop(movie_id)
 
 
 @app.get("/")
@@ -17,18 +30,23 @@ def get_movies():
     """
     Get the list of the **movies** in the database.
     """
-    return movies
+    return list(movies.values())
 
 
 # TODO: Descricao dos argumentos usando Body ou Field, colocar summary e description no .post()
 @app.post("/movies/", tags=["movies"])
-def create_movie(movie: Movie) -> int:
+def create_movie(
+    movie: Annotated[Movie, Body(description="Movie JSON to add to the database")],
+    response: Response,
+) -> MovieDB:
     """
     Receive a **Movie** object as json and add it to the database, returns the created movie id on database.
     """
-    movie_id = len(movies)
-    movies.append(MovieDB(**movie.dict(), movie_id=movie_id, reviews=[]))
-    return movie_id
+    movie_to_add = MovieDB(**movie.dict(), movie_id=last_id)
+    movie_id = add_movie(movie_to_add)
+    response.status_code = status.HTTP_201_CREATED
+    response.headers["Location"] = f"/movies/{movie_id}"
+    return movie_to_add
 
 
 @app.get("/movies/{movie_id}", tags=["movies"])
@@ -38,7 +56,7 @@ def get_movie(
     """
     Get a **Movie** object from the database, given its id.
     """
-    if movie_id >= len(movies):
+    if movie_id not in movies:
         raise HTTPException(
             status_code=404,
             detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
@@ -48,12 +66,14 @@ def get_movie(
 
 @app.get("/reviews/{movie_id}", tags=["reviews"])
 def get_reviews(
-    movie_id: Annotated[int, Path(title="The ID of the movie to get the reviews", ge=0)]
+    movie_id: Annotated[
+        int, Path(title="The ID of the movie to get the all the reviews", ge=0)
+    ]
 ) -> list[movieReview]:
     """
     Get the list of **reviews** for a given movie.
     """
-    if movie_id >= len(movies):
+    if movie_id not in movies:
         raise HTTPException(
             status_code=404,
             detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
@@ -67,7 +87,8 @@ def add_review(
         int, Path(title="The ID of the movie to post the review.", ge=0)
     ],
     review: movieReview,
-) -> list[movieReview]:
+    response: Response,
+) -> movieReview:
     """
     Add a **review** to a given movie.
     """
@@ -77,5 +98,6 @@ def add_review(
             detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
         )
     movies[movie_id].reviews.append(review)
-    # TODO: Rever se esse return faz sentido.
-    return movies[movie_id].reviews
+    response.status_code = status.HTTP_201_CREATED
+    response.headers["Location"] = f"/reviews/{movie_id}"
+    return review
