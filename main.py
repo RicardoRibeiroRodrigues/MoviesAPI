fe4c461e37728e3f3a45395be638658a99a7acec
Guilherme Fontana <guifl2001@hotmail.com>
2023-04-28 10:33:07 -0300

    alterações da main

--- main.py
+++ main.py
@@ -1,36 +1,13 @@
 from typing import Annotated
-from models import *
-from fastapi import FastAPI, Path, HTTPException, Response, status, Body
+from schemas import *
+from fastapi import FastAPI, Path, HTTPException, Response, status, Body, Depends
+from sqlalchemy.orm import Session
+import crud, models, schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-movies: dict[int, MovieDB] = {}
-last_movie_id = 0
-last_review_id = 0
-
-
-def add_movie_db(movie: MovieDB) -> int:
-    global last_movie_id
-    movie_id = last_movie_id
-    movies[movie_id] = movie
-    last_movie_id += 1
-    return movie_id
-
-
-def remove_movie_db(movie_id: int) -> MovieDB:
-    return movies.pop(movie_id)
-
-
-def add_review_db(movie_id: int, review: movieReviewDB) -> int:
-    global last_review_id
-    review_id = last_review_id
-    movies[movie_id].reviews[review_id] = review
-    last_review_id += 1
-    return review_id
-
-
-def remove_review_db(movie_id: int, review_id: int) -> movieReviewDB:
-    return movies[movie_id].reviews.pop(review_id)
 
 
 # This is used to document the 404 response for all endpoints that can raise it.
@@ -41,198 +18,229 @@ RESPONSES_NOT_FOUND = {
 }
 
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 async def root():
     return "Hello World, please check the /docs for info on using this API."
 
 
+@app.post("/users/", tags=["users"], status_code=201)
+def create_user(
+    user: Annotated[UserCreate, Body(description="User JSON to add to the database")],
+    response: Response,
+    db: Session = Depends(get_db),
+) -> schemas.User:
+    """
+    Receive a **User** object as json and add it to the database, returns the created user id on database.
+    """
+    user = crud.create_user(db, user)
+    response.status_code = status.HTTP_201_CREATED
+    response.headers["Location"] = f"/users/{user.user_id}"
+    return user
+
+
 @app.get("/movies/", tags=["movies"])
-def get_movies():
+def get_movies(db: Session = Depends(get_db)) -> list[schemas.Movie]:
     """
     Get the list of the all **movies** in the database.
     """
-    return list(movies.values())
+    return crud.get_movies(db)
 
 
 @app.post("/movies/", tags=["movies"], status_code=201)
 def create_movie(
-    movie: Annotated[Movie, Body(description="Movie JSON to add to the database")],
+    movie: Annotated[
+        MovieCreate, Body(description="Movie JSON to add to the database")
+    ],
     response: Response,
-) -> MovieDB:
+    db: Session = Depends(get_db),
+) -> schemas.Movie:
     """
     Receive a **Movie** object as json and add it to the database, returns the created movie id on database.
     """
-    movie_to_add = MovieDB(**movie.dict(), movie_id=last_movie_id)
-    movie_id = add_movie_db(movie_to_add)
+    movie = crud.create_movie(db, movie)
     response.status_code = status.HTTP_201_CREATED
-    response.headers["Location"] = f"/movies/{movie_id}"
-    return movie_to_add
+    response.headers["Location"] = f"/movies/{movie.movie_id}"
+    return movie
 
 
 @app.get("/movies/{movie_id}", tags=["movies"], responses=RESPONSES_NOT_FOUND)
 def get_movie(
     movie_id: Annotated[
-        int, Path(description="The ID of the movie to get", ge=0, example=0)
-    ]
-) -> MovieDB:
+        int, Path(description="The ID of the movie to get", ge=0, example=1)
+    ],
+    db: Session = Depends(get_db),
+) -> schemas.Movie:
     """
     Get a **Movie** object from the database, given its id.
     """
-    if movie_id not in movies:
+    movie = crud.get_movie(db, movie_id)
+    if not movie:
         raise HTTPException(
             status_code=404,
             detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
         )
-    return movies[movie_id]
+    return movie
 
 
 @app.delete("/movies/{movie_id}", tags=["movies"], responses=RESPONSES_NOT_FOUND)
 def delete_movie(
     movie_id: Annotated[
-        int, Path(description="The ID of the movie to delete", ge=0, example=0)
-    ]
-) -> MovieDB:
+        int, Path(description="The ID of the movie to delete", ge=0, example=1)
+    ],
+    db: Session = Depends(get_db),
+) -> schemas.Movie:
     """
     Delete a **Movie** object from the database, given its id.
     """
-    if movie_id not in movies:
+    movie = crud.delete_movie(db, movie_id)
+    if not movie:
         raise HTTPException(
             status_code=404,
             detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
         )
-    return remove_movie_db(movie_id)
+    return movie
 
 
 @app.put("/movies/{movie_id}", tags=["movies"], responses=RESPONSES_NOT_FOUND)
 def update_movie(
     movie_id: Annotated[
-        int, Path(description="The ID of the movie to update", ge=0, example=0)
+        int, Path(description="The ID of the movie to update", ge=0, example=1)
     ],
-    movie: Annotated[Movie, Body(description="Movie JSON to update the database")],
-) -> MovieDB:
+    movie: Annotated[
+        schemas.MovieCreate, Body(description="Movie JSON to update the database")
+    ],
+    db: Session = Depends(get_db),
+) -> schemas.Movie:
     """
     Update a **Movie** object from the database, given its id.
     """
-    if movie_id not in movies:
+    movie_updated = crud.update_movie(db, movie_id, movie)
+    if not movie_updated:
         raise HTTPException(
             status_code=404,
             detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
         )
-    movies[movie_id] = MovieDB(**movie.dict(), movie_id=movie_id)
-    return movies[movie_id]
+    return movie_updated
 
 
-@app.get("/movies/{movie_id}/reviews", tags=["reviews"], responses=RESPONSES_NOT_FOUND)
+@app.get("/reviews/{movie_id}", tags=["reviews"], responses=RESPONSES_NOT_FOUND)
 def get_movie_reviews(
     movie_id: Annotated[
         int,
         Path(
-            description="The ID of the movie to get the all the reviews",
+            description="The id of the movie to get the all the reviews",
             ge=0,
-            example=0,
+            example=1,
         ),
-    ]
-) -> list[movieReview]:
+    ],
+    db: Session = Depends(get_db),
+) -> list[schemas.MovieReview]:
     """
     Get the list of **reviews** for a given movie.
     """
-    if movie_id not in movies:
+    movie_reviews = crud.get_movie_reviews(db, movie_id)
+    if not movie_reviews:
         raise HTTPException(
             status_code=404,
             detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
         )
-    return list(movies[movie_id].reviews.values())
+    return movie_reviews
 
 
 @app.post(
-    "/movies/{movie_id}/reviews",
+    "/reviews/",
     tags=["reviews"],
     status_code=201,
     responses=RESPONSES_NOT_FOUND,
 )
 def create_review(
-    movie_id: Annotated[
-        int,
-        Path(description="The ID of the movie to post the review.", ge=0, example=0),
-    ],
-    review: movieReview,
+    review: schemas.MovieReviewCreate,
     response: Response,
-) -> movieReview:
+    db: Session = Depends(get_db),
+) -> schemas.MovieReview:
     """
     Add a **review** to a given movie.
     """
-    if movie_id not in movies:
+    if not crud.get_movie(db, review.movie_id):
         raise HTTPException(
             status_code=404,
-            detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
+            detail=f"The movie with id {review.movie_id} doesnt exist in the DataBase.",
         )
-    movie_id = add_review_db(
-        movie_id, movieReviewDB(**review.dict(), review_id=last_review_id)
+    elif not crud.get_user(db, review.user_id):
+        raise HTTPException(
+            status_code=404,
+            detail=f"The user with id {review.user_id} doesnt exist in the DataBase.",
+        )
+
+    moview_review = crud.create_movie_review(
+        db, review, review.user_id, review.movie_id
     )
+    if not moview_review:
+        raise HTTPException(
+            status_code=404,
+            detail=f"The movie with id {review.movie_id} doesnt exist in the DataBase.",
+        )
     response.status_code = status.HTTP_201_CREATED
-    response.headers["Location"] = f"/reviews/{movie_id}"
-    return review
+    response.headers["Location"] = f"/reviews/{moview_review.review_id}"
+    return moview_review
 
 
 @app.put(
-    "/movies/{movie_id}/reviews/{review_id}",
+    "/reviews/{review_id}",
     tags=["reviews"],
     responses=RESPONSES_NOT_FOUND,
 )
 def update_review(
-    movie_id: Annotated[
-        int,
-        Path(description="The id of the movie to update the review.", ge=0, example=0),
-    ],
     review_id: Annotated[
-        int, Path(description="The id of the review to update.", ge=0, example=0)
+        int, Path(description="The id of the review to update.", ge=0, example=1)
     ],
-    review: movieReview,
-) -> movieReview:
+    review: schemas.MovieReviewCreate,
+    db: Session = Depends(get_db),
+) -> schemas.MovieReview:
     """
     Update a **review** from its id and the **movie id**.
     """
-    if movie_id not in movies:
+    if not crud.get_movie(db, review.movie_id):
         raise HTTPException(
             status_code=404,
-            detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
+            detail=f"The movie with id {review.movie_id} doesnt exist in the DataBase.",
         )
-    if review_id not in movies[movie_id].reviews:
+    review_updated = crud.update_movie_review(db, review_id, review)
+    if not review_updated:
         raise HTTPException(
             status_code=404,
             detail=f"The review with id {review_id} doesnt exist in the DataBase.",
         )
-    movies[movie_id].reviews[review_id] = movieReviewDB(
-        **review.dict(), review_id=review_id
-    )
-    return review
+    return review_updated
 
 
 @app.delete(
-    "/movies/{movie_id}/reviews/{review_id}",
+    "/reviews/{review_id}",
     tags=["reviews"],
     responses=RESPONSES_NOT_FOUND,
 )
 def delete_review(
-    movie_id: Annotated[
-        int,
-        Path(description="The ID of the movie to delete the review.", ge=0, example=0),
-    ],
     review_id: Annotated[
         int, Path(description="The ID of the review to delete.", ge=0, example=0)
     ],
-) -> movieReview:
+    db: Session = Depends(get_db),
+) -> schemas.MovieReview:
     """
     Delete a **review** from a given movie.
     """
-    if movie_id not in movies:
-        raise HTTPException(
-            status_code=404,
-            detail=f"The movie with id {movie_id} doesnt exist in the DataBase.",
-        )
-    if review_id not in movies[movie_id].reviews:
+    deleted_review = crud.delete_movie_review(db, review_id)
+    if not deleted_review:
         raise HTTPException(
             status_code=404,
             detail=f"The review with id {review_id} doesnt exist in the DataBase.",
         )
-    return remove_review_db(movie_id, review_id)
+    return deleted_review
